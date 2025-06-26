e19d688022f09d4a58b7a49d6f310f770603570d
Nayleen <almightynay@gmail.com>
2025-06-27 01:03:45 +0200

    fix: another attempt at fixing the annotations

--- tools/base-image-digest.sh
+++ tools/base-image-digest.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-if [ "$#" -lt 1 ]; then
-  echo "Usage: $0 <image> <?base_image>" >&2
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <image>" >&2
   exit 1
 fi
 
@@ -20,20 +20,6 @@ CACHEFILE="/tmp/${IMAGE//\//_}.json"
 
 if [ ! -f "$CACHEFILE" ]; then
   docker buildx imagetools inspect "$IMAGE" --format "{{json .}}" > $CACHEFILE
-fi
-
-if [ ! -z "$BASE_IMAGE" ]; then
-  EXPECTED_BASE_IMAGE=$(jq -r ".manifest.manifests.[] | select(.platform.os == \"linux\" and .platform.architecture == \"$ARCH\") | .annotations | .\"org.opencontainers.image.base.name\"" $CACHEFILE)
-
-  if [ "$EXPECTED_BASE_IMAGE" = "null" ]; then
-    echo "Image $IMAGE does not have a org.opencontainers.image.base.name annotation for architecture: $ARCH" >&2
-    exit 1
-  fi
-
-  if [[ "$BASE_IMAGE" != "$EXPECTED_BASE_IMAGE" && "$BASE_IMAGE" != *"$EXPECTED_BASE_IMAGE" ]]; then
-    echo "Image $IMAGE does not have the expected base image: $BASE_IMAGE" >&2
-    exit 1
-  fi
 fi
 
 BASE_IMAGE_DIGEST=$(jq -r ".manifest.manifests.[] | select(.platform.os == \"linux\" and .platform.architecture == \"$ARCH\") | .annotations | .\"org.opencontainers.image.base.digest\"" $CACHEFILE)
